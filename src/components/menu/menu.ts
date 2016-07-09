c3c7f6d2ceca6aff8729a40f061f1d2d4f9d2bfb
Marcel Schmidt <derprogger@gmail.com>
2016-07-09 21:14:43 +0200

    MdMenu

--- src/components/menu/menu.ts
+++ src/components/menu/menu.ts
@@ -3,11 +3,31 @@ import {Component, ViewEncapsulation} from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'md-menu',
+  host: {
+    '(click)': 'toggle()',
+    '[class.show-menu]': 'visible'
+  },
   templateUrl: 'menu.html',
   styleUrls: ['menu.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class MdMenu {}
+export class MdMenu {
+  visible: boolean = false;
+  toggle() {
+    this.visible = !this.visible;
+  }
+}
 
-export const MD_MENU_DIRECTIVES = [MdMenu];
+@Component({
+  moduleId: module.id,
+  selector: 'md-menu-content',
+  template: '<ng-content></ng-content>',
+  styleUrls: ['menu.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MdMenuContent  {
+
+}
+
+export const MD_MENU_DIRECTIVES = [MdMenu,MdMenuContent];
 
--- src/demo-app/menu/menu-demo.ts
+++ src/demo-app/menu/menu-demo.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
 import {MD_MENU_DIRECTIVES} from '@angular2-material/menu/menu';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'menu-demo',
   templateUrl: 'menu-demo.html',
   styleUrls: ['menu-demo.css'],
-  directives: [MD_MENU_DIRECTIVES]
+  directives: [MD_MENU_DIRECTIVES,ROUTER_DIRECTIVES]
 })
-export class MenuDemo {}
+export class MenuDemo {
+  action(){
+window.alert('click');
+  }
+}
